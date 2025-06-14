db2b6af11e5e8dc7709370bab6c2ed5b704fef5f
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2025-06-14 12:30:00 +0000

    Foi criado a exibição das coodenadas dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -15,6 +15,13 @@ int main() {
     tabuleiro[4][5] = 1;
     tabuleiro[4][6] = 1;
 
+    //Exibindo as coodenadas dos navios
+    printf("Exibindo as coodenadas dos navios:\n\n");
+    printf("Navio na vertical\n");
+    printf("(1,1)\n(2,1)\n(3,1)\n\n");
+    printf("Navio horizontal\n");
+    printf("(4,4)\n(4,5)\n(4,6)\n\n");
+
 
     return 0;
 }
