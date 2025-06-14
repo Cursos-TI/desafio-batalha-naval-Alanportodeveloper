3771d4262f4a8dd4a61df28156b586edcdbc5da9
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2025-06-14 12:19:04 +0000

    Posicionando os navios na vertical e horinzontal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,7 +3,18 @@
 int main() {
 
     //Declarando o tabuleiro
-    int tabuleiro [10] [10];
+    int tabuleiro [10][10];
+
+    //posiciona um navio na vertical
+    tabuleiro[1][1] = 1;
+    tabuleiro[2][1] = 1;
+    tabuleiro[3][1] = 1;
+
+    //posiciona um navio na horizontal
+    tabuleiro[4][4] = 1;
+    tabuleiro[4][5] = 1;
+    tabuleiro[4][6] = 1;
+
 
     return 0;
 }
